a552b1082d6a9b7d26dc91ea726bfe63e5f087e9
Nick Sayago <nick_s7@aol.com>
2020-12-10 23:54:42 -0500

    working

--- src/App.js
+++ src/App.js
@@ -1,71 +1,101 @@
-import React, { Component } from 'react'
-import './App.css';
-import axios from 'axios'
-import CategoryList from './components/CategoryList.js'
-import { Switch, Route } from 'react-router-dom';
-import MealsPage from './components/MealsPage.js';
-import MealDetails from './components/MealDetails.js';
-import SearchBar from './components/SearchBar.js';
-import Navbar from './components/NavBar'
-
+import React, { Component } from "react";
+import "./App.css";
+import axios from "axios";
+import CategoryList from "./components/CategoryList.js";
+import { Switch, Route } from "react-router-dom";
+import MealsPage from "./components/MealsPage.js";
+import MealDetails from "./components/MealDetails.js";
+import SearchBar from "./components/SearchBar.js";
+import Navbar from "./components/NavBar";
 
 export default class App extends Component {
-
-  state ={
+  state = {
     categoryList: [],
     category: "",
-    selectedCategory:"",
-    meal:"",
-    selectedMeal:"",
-  }
+    selectedCategory: "",
+    meal: "",
+    selectedMeal: "",
+  };
 
   componentDidMount() {
-    axios.get('https://www.themealdb.com/api/json/v1/1/categories.php')
-    .then(response => {
-        console.log(response)
-        this.setState({categoryList: response.data.categories})
-    })
-    .catch(error => {
-      // handle error
-      console.log(error);
-    });
+    axios
+      .get("https://www.themealdb.com/api/json/v1/1/categories.php")
+      .then((response) => {
+        console.log(response);
+        this.setState({ categoryList: response.data.categories });
+      })
+      .catch((error) => {
+        // handle error
+        console.log(error);
+      });
+  }
+  //working
 
+  handleCategories = (category) => {
+    this.setState({ selectedCategory: category });
+  };
 
-}
-
-handleCategories = (category) => {
-  this.setState({selectedCategory: category})
-}
-
-handleMeals = (meal) => {
-  this.setState({selectedMeal : meal})
-}
-
-
+  handleMeals = (meal) => {
+    this.setState({ selectedMeal: meal });
+  };
 
   render() {
-
-
     return (
       <div className="App">
-  <div className="app-background">
-      <Navbar/>
-      
+        <div className="app-background">
+          <Navbar />
 
-        <Switch>
+          <Switch>
+            <Route
+              exact
+              path="/"
+              render={(props) => (
+                <CategoryList
+                  {...props}
+                  handleCategories={this.handleCategories}
+                  categoryList={this.state.categoryList}
+                  selectedCategory={this.state.selectedCategory}
+                />
+              )}
+            />
 
-          <Route exact path='/' render={ (props) => <CategoryList {...props} handleCategories = {this.handleCategories} categoryList={this.state.categoryList} selectedCategory={this.state.selectedCategory}/>} />
+            <Route
+              exact
+              path={`/meals/${this.state.selectedCategory}`}
+              render={(props) => (
+                <MealsPage
+                  {...props}
+                  handleMeals={this.handleMeals}
+                  selectedCategory={this.state.selectedCategory}
+                />
+              )}
+            />
 
-          <Route exact path={`/meals/${this.state.selectedCategory}`} render={(props) => <MealsPage  {...props} handleMeals = {this.handleMeals} selectedCategory={this.state.selectedCategory}/> }/>
+            <Route
+              exact
+              path={`/meals:${this.state.selectedMeal}`}
+              render={(props) => (
+                <MealDetails
+                  {...props}
+                  selectedMeal={this.state.selectedMeal}
+                />
+              )}
+            />
 
-          <Route exact path={`/meals:${this.state.selectedMeal}`} render={(props) => <MealDetails {...props} selectedMeal={this.state.selectedMeal}/>}/>
-
-          <Route exact path='/search' render={(props) => <SearchBar {...props} handleMeals = {this.handleMeals} selectedMeal={this.state.selectedMeal}/>}/>
-
-        </Switch>
-  </div>
-
+            <Route
+              exact
+              path="/search"
+              render={(props) => (
+                <SearchBar
+                  {...props}
+                  handleMeals={this.handleMeals}
+                  selectedMeal={this.state.selectedMeal}
+                />
+              )}
+            />
+          </Switch>
+        </div>
       </div>
-    )
+    );
   }
 }
